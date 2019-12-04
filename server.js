611c40f984cb5a08868213bd09d32d9aec9198e7
codesudarshan <sudarshan.shinde@ves.ac.in>
2019-12-04 19:29:01 +0530

    for heroku

--- server.js
+++ server.js
@@ -1,20 +1,6 @@
-//Install express server
 const express = require('express');
-const path = require('/');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/sitevankon'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/sitevankon/index.html'));
-});
-
-app.set( 'port', ( process.env.PORT || 8080 ));
-
-// Start the app by listening on the default Heroku port
-app.listen( app.get( 'port' ), function() {
-    console.log( 'Node server is running on port ' + app.get( 'port' ));
-    });
+app.listen(process.env.PORT || 8080);
