ad055ade8ce1490a26a7c965266a24682b6d707f
Sudarshan Shinde <sudarshan.shinde@ves.ac.in>
2019-12-31 02:50:45 +0530

    Account management

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -5,6 +5,7 @@ import { User } from 'app/shared/models/User';
 import { Response } from 'app/shared/models/Response';
 import { RvUser } from 'app/shared/models/RvUser';
 import { UserKyc } from 'app/shared/models/UserKyc';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'user-cmp',
@@ -25,15 +26,20 @@ export class AccountComponent implements OnInit{
     responseMessage:string;
     userAction:string = 'Create User';
     btnStatusClass:string = 'btn btn-primary';
+    panCard:any;
+    aadharCard:any;
+    self:any;
+    hide:boolean=false;
 
     sort(key){
         this.key = key;
         this.reverse = !this.reverse;
     }
 
-    constructor(public http: HttpClient){
+    constructor(public http: HttpClient, public domSanitizor:DomSanitizer){
        
         this.displayProducts();
+        this.hide = false;
     }
 
     ngOnInit(){
@@ -67,12 +73,53 @@ export class AccountComponent implements OnInit{
         this.userAction = "Update User";
         this.user = item;
     }
+
+    approveKyc(){
+
+    }
+    RejectKyc(){
+
+    }
     viewKyc(item:RvUser){
         this.http.get(this.showKycUrl+'/'+item.mobileNumber).subscribe(
             (data:UserKyc) => {
+                if(data != null){
               this.userKyc = data;
-              console.log(this.userKyc);
+                this.hide = true;
+                this.panCard = null;
+                this.aadharCard = null;
+                this.self = null;
+                this.showImage(data.panCardEncodedImage,'panCard');
+                this.showImage(data.adharCardEncodedImage,'aadharCard');
+                this.showImage(data.userProfileImage,'userProfile');
+                }else
+                {
+                    this.hide = false;
+                    window.alert('Kyc documents not yet uploaded');
+                }
+              //this.panCard ='data:image/jpeg;base64,'+ data.panCardEncodedImage;
+
+            //   const reader = new FileReader();
+            //   reader.onload = (e) => this.panCard = e.target.result;
+            //   reader.readAsDataURL(new Blob([data.panCardEncodedImage]));
+               console.log(this.userKyc);
             });
+    }
+
+    showImage(imageData:any,imageType){
+        let TYPED_ARRAY = new Uint8Array(imageData);
+        const STRING_CHAR = String.fromCharCode.apply(null, TYPED_ARRAY);
+        let base64String = btoa(STRING_CHAR);
+        if(imageType === 'panCard'){
+            this.panCard = this.domSanitizor.bypassSecurityTrustUrl('data:image/jpg;base64,'+base64String);
+        }
+        if(imageType === 'aadharCard'){
+            this.aadharCard = this.domSanitizor.bypassSecurityTrustUrl('data:image/jpg;base64,'+base64String);
+        }
+        if(imageType === 'userProfile'){
+            this.self = this.domSanitizor.bypassSecurityTrustUrl('data:image/jpg;base64,'+base64String);
+        }
+        
     }
     changeStatus(item:RvUser){
         if(item.active){
